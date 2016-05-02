482abc0bde7f94b93b59520e57d11107beddf23b
Muhammad Vaid <molvy32@gmail.com>
2016-05-02 18:20:56 +0200

    More code cleanup. Check comments please. Some changes are needed

--- Test.cpp
+++ Test.cpp
@@ -27,10 +27,9 @@ int gameState = 1;																		//initial gamestate is menu
 extern int width;
 extern int height;
 int numAlive = NUM_COLUMNS*NUM_ROWS;
-int Current_EnemyCount = NUM_COLUMNS*NUM_ROWS;
 int EnemyWaveCount = 0;
 int input=0;
-char* mistring;
+char* nameString;
 
 bool isHighscore = false;																//READ IN LOWEST HIGHSCORE FIRST. USE THIS BOOL TO CHECK USER FINAL SCORE VS LOWEST HIGHSCORE
 int lowScore = 0;
@@ -50,7 +49,7 @@ void CollideBarrier();
 void Reactivate_Enemies();
 void EnemyReachEnd();
 void BulletBarrierCollide();
-void InitialiseBarriers();
+void InitBarriers();
 void Reactivate_Barriers();
 void updateEnemyCount();
 
@@ -59,26 +58,13 @@ Spaceship	player(width/2, height*4/5);
 Bullet		playerBullet(player.x_pos, player.y_pos,10,true);
 Bullet		enemyBullet(0,0,10,false);
 Enemy		arrEnem[NUM_COLUMNS][NUM_ROWS];												//array of objects
-Barrier		Asteroid[3];
-
+Barrier		redBarrier[3];
 BackGround BG;
 BackGround MG;
 BackGround FG;
 BackGround MM;
 
-// \/ MOVE TO CLASS \/
-struct MovingBackground
-{
-	float x;
-	float y;
-	float velX;
-	float velY;
-	int dirX;
-	int dirY;
-	int width;
-	int height;
-	ALLEGRO_BITMAP *BG;
-};
+BackGround MBG;				//<---------------- what is this for?? Unused
 
 
 int main(void)
@@ -87,18 +73,15 @@ int main(void)
 	enemyBullet.isFriendly = false;
 	srand((unsigned)time(NULL));
 
-	//primitive variables
+	//variables
 	const int FPS = 60;
 	bool done = false;
 	bool redraw = true;
-	const int frames = 28;
 	int CurrentFrame = 0;
-	int Delay = 10;
-
-	InitialiseBarriers();
-
-	MovingBackground MBG;
+	const int frames = 28;
 	
+	// \/ no need to use variable 
+	int Delay = 10;					
 
 
 	if(!al_init())
@@ -107,10 +90,6 @@ int main(void)
 		return -1;
 	}
 	
-	ALLEGRO_BITMAP *bgImage = NULL;
-	ALLEGRO_BITMAP *mgImage = NULL;
-	ALLEGRO_BITMAP *fgImage = NULL;
-
 	//Allegro variables
 	ALLEGRO_DISPLAY *DISPLAY = NULL;
 	ALLEGRO_BITMAP *picHealth[7];
@@ -119,10 +98,11 @@ int main(void)
 	ALLEGRO_BITMAP *picEnemy = NULL;
 	ALLEGRO_BITMAP *Game = NULL;
 	ALLEGRO_BITMAP *MENU = NULL;
+	ALLEGRO_BITMAP *bgImage = NULL;
+	ALLEGRO_BITMAP *mgImage = NULL;
+	ALLEGRO_BITMAP *fgImage = NULL;
 	ALLEGRO_BITMAP *MenuAnim[frames];
-	
-	
-	
+
 	ALLEGRO_EVENT_QUEUE *TestQueue = NULL;
 	ALLEGRO_TIMER *timer = NULL;
 	ALLEGRO_FONT *font25 = NULL;
@@ -148,18 +128,19 @@ int main(void)
 	al_install_audio();
 	al_init_acodec_addon();
 
-	al_reserve_samples(4);
-	blaster = al_load_sample("XWing-Laser.ogg");
-	explosion = al_load_sample("Blast.ogg");
-	startGame = al_load_sample("xwing.ogg");
-	music = al_load_sample("Star_Wars.ogg");
-
 	if (!DISPLAY)
 	{
 		al_show_native_message_box(NULL, NULL, NULL, "Could not initialize display", NULL, NULL);
 		return -1;
 	}
 
+	//load sounds
+	al_reserve_samples(4);
+	blaster = al_load_sample("XWing-Laser.ogg");
+	explosion = al_load_sample("Blast.ogg");
+	startGame = al_load_sample("xwing.ogg");
+	music = al_load_sample("Star_Wars.ogg");
+	
 	//Load Pictures
 	picBullet=al_load_bitmap("Lazer.png");
 	picShip =al_load_bitmap("player1.png");
@@ -172,11 +153,15 @@ int main(void)
 	Barrier[2] = al_load_bitmap("B2.png");
 	Barrier[3] = al_load_bitmap("B1.png");
 	Barrier[4] = al_load_bitmap("B0.png");
-
 	for (int i = 0; i < 5; i++)
 		al_convert_mask_to_alpha(Barrier[i], al_map_rgb(255, 255, 255));
 
+	AsImage[0] = Barrier[4];
+	AsImage[1] = Barrier[4];
+	AsImage[2] = Barrier[4];
 
+
+	// \/ Remove \/
 	MenuAnim[0] = al_load_bitmap("S1.png");
 	MenuAnim[1] = al_load_bitmap("S2.png");
 	MenuAnim[2] = al_load_bitmap("S3.png");
@@ -205,17 +190,10 @@ int main(void)
 	MenuAnim[25] = al_load_bitmap("S26.png");
 	MenuAnim[26] = al_load_bitmap("S27.png");
 	MenuAnim[27] = al_load_bitmap("S28.png");
-
-	for (int i = 0; i < 28; i++) {
-   	al_convert_mask_to_alpha(MenuAnim[i], al_map_rgb(239, 241, 240));
+	for (int i = 0; i < 28; i++) 
+   		al_convert_mask_to_alpha(MenuAnim[i], al_map_rgb(239, 241, 240));
+	 
 	
-	 }
-
-
-	AsImage[0] = Barrier[4];
-	AsImage[1] = Barrier[4];
-	AsImage[2] = Barrier[4];
-
 	picHealth[0] = al_load_bitmap("1.png");
 	picHealth[1] = al_load_bitmap("2.png");
 	picHealth[2] = al_load_bitmap("3.png");
@@ -223,14 +201,14 @@ int main(void)
 	picHealth[4] = al_load_bitmap("5.png");
 	picHealth[5] = al_load_bitmap("6.png");
 	picHealth[6] = al_load_bitmap("blank.png");
+	for (int i = 0; i < 7; i++)
+		al_convert_mask_to_alpha(picHealth[i], al_map_rgb(0, 0, 0));
 
 	bgImage = al_load_bitmap("starBG.png");
 	mgImage = al_load_bitmap("starMG.jpg");
 	fgImage = al_load_bitmap("starFG.png");
 
-	for (int i = 0; i < 7; i++) {
-		al_convert_mask_to_alpha(picHealth[i], al_map_rgb(0, 0, 0));
-	}
+		
 	al_convert_mask_to_alpha(picShip,al_map_rgb(0,0,0));
 	al_convert_mask_to_alpha(picEnemy, al_map_rgb(0, 0, 0));
 	al_convert_mask_to_alpha(picBullet, al_map_rgb(0, 0, 0));
@@ -240,25 +218,25 @@ int main(void)
 	InitBackground(MG, 0, 0, 3, 0, 2000, 768, -1, 1, mgImage);
 	InitBackground(FG, 0, 0, 5, 0, 800, 600, -1, 1, fgImage);
 	InitBackground(MM, 0, 0, 1, 0, 800, 600, -1, 1,MENU);
+	InitBarriers();
+
+	timer = al_create_timer(1.0 / FPS);
 
 	al_set_display_icon(DISPLAY, picShip);
-
-	timer=al_create_timer(1.0/FPS);
+	al_play_sample(music, 1, 0, 1, ALLEGRO_PLAYMODE_LOOP, NULL);
 
 	TestQueue= al_create_event_queue();
 	al_register_event_source(TestQueue,al_get_keyboard_event_source());
 	al_register_event_source(TestQueue,al_get_display_event_source(DISPLAY));
 	al_register_event_source(TestQueue,al_get_timer_event_source(timer));
-	
 	al_start_timer(timer);
+
 	int score = 0;
 	int frameCount = 0;
 	int pos = (int)al_ustr_size(str);
 	font25 = al_load_font("Legacy.ttf", 38, 0);
 	font50 = al_load_font("STARWARS.TTF", 55, 0);
 	fontName = al_load_font("Legacy.ttf", 38, 0);
-	
-	al_play_sample(music, 1, 0, 1, ALLEGRO_PLAYMODE_LOOP, NULL);
 	
 	while (!done)
 	{
@@ -381,21 +359,18 @@ int main(void)
 			redraw = false;
 			if (gameState == 1)		//menu
 			{
-				 DrawBackground(MM);
-
-				 	if (++CurrentFrame >= Delay) {
-
-				 if (++CurrentFrame >= frames) {
+				DrawBackground(MM);
+				if (++CurrentFrame >= Delay) 
+				{
+					if (++CurrentFrame >= frames) 
+					{
 				 		CurrentFrame = 0;
-				 	frameCount = 0;
-
+				 		frameCount = 0;
 				 	}
-				 	}
+				 }
 
+				al_draw_bitmap(MenuAnim[CurrentFrame], 820, 500, 0); //to be removed
 
-				 al_draw_bitmap(MenuAnim[CurrentFrame], 820, 500, 0);
-
-				
 				al_draw_text(font25, al_map_rgb(255, 40, 78), width / 2, height- 750, ALLEGRO_ALIGN_CENTRE, "AMMST  PRESENTS");
 				al_draw_text(font50, al_map_rgb(255, 40, 78), (width / 2), (height) - 690, ALLEGRO_ALIGN_CENTRE, "DARTH   INVADERS");
 				al_draw_text(font25, al_map_rgb(255, 40, 78), (width / 2), (height) - 350, ALLEGRO_ALIGN_CENTRE, "PRESS SPACE TO START");
@@ -411,29 +386,28 @@ int main(void)
 				// Make into method \/
 				for (int b = 0; b < 3; b++)
 				{
-					if (Asteroid[b].life_points != -1)
+					if (redBarrier[b].life_points != -1)
 					{
-						AsImage[b] = Barrier[Asteroid[b].life_points];
+						AsImage[b] = Barrier[redBarrier[b].life_points];
 					}
 
-					if (Asteroid[b].life_points == 5)
+					if (redBarrier[b].life_points == 5)
 						AsImage[b] = Barrier[4];
-					if (Asteroid[b].life_points == 4)
+					if (redBarrier[b].life_points == 4)
 						AsImage[b] = Barrier[3];
-					if (Asteroid[b].life_points == 3)
+					if (redBarrier[b].life_points == 3)
 						AsImage[b] = Barrier[2];
-					if (Asteroid[b].life_points == 2)
+					if (redBarrier[b].life_points == 2)
 						AsImage[b] = Barrier[1];
-					if (Asteroid[b].life_points == 1)
+					if (redBarrier[b].life_points == 1)
 						AsImage[b] = Barrier[0];
 				}
 
-
-				if (Asteroid[0].active == true)
+				if (redBarrier[0].active == true)
 					al_draw_bitmap(AsImage[0], 50, 500, 0);
-				if (Asteroid[1].active == true)
+				if (redBarrier[1].active == true)
 					al_draw_bitmap(AsImage[1], 435, 500, 0);
-				if (Asteroid[2].active == true)
+				if (redBarrier[2].active == true)
 					al_draw_bitmap(AsImage[2], 820, 500, 0);
 
 
@@ -517,16 +491,16 @@ int main(void)
 				if(score>lowScore)
 					isHighscore = true; 
 
-				mistring = al_cstr_dup(str);
+				nameString = al_cstr_dup(str);
 				al_draw_textf(font50, al_map_rgb(255, 0, 0), width / 2 - 250, height / 2 - 200, 0, "FINAL SCORE: %i", score);
 
 				if(isHighscore)
-					al_draw_text(fontName, al_map_rgb_f(1, 1, 1), width / 2 - 250, height / 2 - 100, ALLEGRO_ALIGN_LEFT, mistring);
+					al_draw_text(fontName, al_map_rgb_f(1, 1, 1), width / 2 - 250, height / 2 - 100, ALLEGRO_ALIGN_LEFT, nameString);
 			}
 
 			else if (gameState == 4)	//highscores
 			{
-				string name = mistring;
+				string name = nameString;
 				//if (isHighscore) -> write name & score to textfile & then SHOW LEADERBOARD here || if not highscore. just show leaderboard
 			}
 
@@ -649,7 +623,7 @@ void collideEnemy(int &score)
 					{
 						playerBullet.status = 0;														//bullet set to not active
 						arrEnem[i][j].active = false;													//enemy set to not active
-						Current_EnemyCount--;
+						
 						switch (j)
 						{
 						case 0:
@@ -796,22 +770,22 @@ void DrawBackground(BackGround &back)
 }
 
 
-void InitialiseBarriers()
+void InitBarriers()
 {
-	Asteroid[0].SetBarrierBound(111, 10, 15);
-	Asteroid[0].SetBarrierpos(50, 500);
-	Asteroid[0].setLife(5);
-	Asteroid[0].active = true;
+	redBarrier[0].SetBarrierBound(111, 10, 15);
+	redBarrier[0].SetBarrierpos(50, 500);
+	redBarrier[0].setLife(5);
+	redBarrier[0].active = true;
 
-	Asteroid[1].SetBarrierBound(111, 10, 15);
-	Asteroid[1].SetBarrierpos(435, 500);
-	Asteroid[1].setLife(5);
-	Asteroid[1].active = true;
+	redBarrier[1].SetBarrierBound(111, 10, 15);
+	redBarrier[1].SetBarrierpos(435, 500);
+	redBarrier[1].setLife(5);
+	redBarrier[1].active = true;
 
-	Asteroid[2].SetBarrierBound(111, 10, 15);
-	Asteroid[2].SetBarrierpos(820, 500);
-	Asteroid[2].setLife(5);
-	Asteroid[2].active = true;
+	redBarrier[2].SetBarrierBound(111, 10, 15);
+	redBarrier[2].SetBarrierpos(820, 500);
+	redBarrier[2].setLife(5);
+	redBarrier[2].active = true;
 }
 
 void Reactivate_Enemies()
@@ -829,17 +803,17 @@ void CollideBarrier()
 {
 	for (int i = 0; i < 3; i++)
 	{
-		if (Asteroid[i].active && enemyBullet.status == 1)
+		if (redBarrier[i].active && enemyBullet.status == 1)
 		{
-			if (enemyBullet.x_pos >(Asteroid[i].x_pos - Asteroid[i].Bleft) && enemyBullet.x_pos<(Asteroid[i].x_pos + Asteroid[i].Bright)
-			&& enemyBullet.y_pos>(Asteroid[i].y_pos - Asteroid[i].BHeight) && enemyBullet.y_pos < (Asteroid[i].y_pos + Asteroid[i].BHeight))
+			if (enemyBullet.x_pos >(redBarrier[i].x_pos - redBarrier[i].Bleft) && enemyBullet.x_pos<(redBarrier[i].x_pos + redBarrier[i].Bright)
+			&& enemyBullet.y_pos>(redBarrier[i].y_pos - redBarrier[i].BHeight) && enemyBullet.y_pos < (redBarrier[i].y_pos + redBarrier[i].BHeight))
 			{
-				if (Asteroid[i].life_points != 0)
+				if (redBarrier[i].life_points != 0)
 				{
-					Asteroid[i].life_points--;
-					if (Asteroid[i].life_points == 0)
+					redBarrier[i].life_points--;
+					if (redBarrier[i].life_points == 0)
 					{
-						Asteroid[i].active = false;
+						redBarrier[i].active = false;
 					}
 				}
 				enemyBullet.status = 0;
@@ -852,10 +826,10 @@ void BulletBarrierCollide()
 {
 	for (int i=0; i < 3; i++)
 	{
-		if (Asteroid[i].active && playerBullet.status == 1)
+		if (redBarrier[i].active && playerBullet.status == 1)
 		{
-			if (playerBullet.x_pos >(Asteroid[i].x_pos - Asteroid[i].Bleft) && playerBullet.x_pos<(Asteroid[i].x_pos + Asteroid[i].Bright)
-				&& playerBullet.y_pos>(Asteroid[i].y_pos - Asteroid[i].BHeight) && playerBullet.y_pos < (Asteroid[i].y_pos + Asteroid[i].BHeight + 17))
+			if (playerBullet.x_pos >(redBarrier[i].x_pos - redBarrier[i].Bleft) && playerBullet.x_pos<(redBarrier[i].x_pos + redBarrier[i].Bright)
+				&& playerBullet.y_pos>(redBarrier[i].y_pos - redBarrier[i].BHeight) && playerBullet.y_pos < (redBarrier[i].y_pos + redBarrier[i].BHeight + 17))
 			{
 				playerBullet.status = 0;
 			}
@@ -866,10 +840,9 @@ void BulletBarrierCollide()
 void updateEnemyCount()
 {
 
-	if (Current_EnemyCount == 0)
+	if (numAlive == 0)
 	{
 		EnemyWaveCount++;
-		Current_EnemyCount = NUM_COLUMNS*NUM_ROWS;
 		numAlive = NUM_COLUMNS*NUM_ROWS;
 		Reactivate_Enemies();
 		Reactivate_Barriers();
@@ -904,7 +877,7 @@ void EnemyReachEnd()
 		{
 			if (arrEnem[i][j].active == true)
 			{
-				if (arrEnem[i][j].y_pos > Asteroid[0].y_pos - Asteroid[0].BHeight)
+				if (arrEnem[i][j].y_pos > redBarrier[0].y_pos - redBarrier[0].BHeight)
 				{
 					gameState = 3;
 				}
@@ -917,8 +890,9 @@ void Reactivate_Barriers()
 {
 	for (int b = 0; b < 3; b++)
 	{
-		Asteroid[b].life_points = 5;
-		Asteroid[b].active = true;
+		redBarrier[b].life_points = 5;
+		redBarrier[b].active = true;
 	}
 
 }
+
